3a0176c25880a3ba74adbfbb1c039ae3684a7f5b
Paulo Canuto <paulohfcanuto@gmail.com>
2017-12-05 11:33:02 -0200

    hora correta

--- config/express.js
+++ config/express.js
@@ -5,19 +5,34 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var routes = require('../app/routes');
 
+morgan.token('localDate', function getDateToken(req, res, format) {
+    var date = new Date();
+    var options = {
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit', 
+        timeZone: 'America/Sao_Paulo'
+    };
+    return date.toLocaleDateString('pt-BR', options);
+});
 
-app.use(morgan('combined'));
-app.use(responseTime({digits: 2}));
+
+app.use(morgan(':remote-addr :localDate ":method :url HTTP/:http-version" :status :response-time ms ":referrer" ":user-agent"'));
+
+app.use(responseTime({ digits: 2 }));
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
